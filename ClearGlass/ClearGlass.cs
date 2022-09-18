572bf6b3547376eae07ca0c3a3599a187ba2929a
JohnCorby <williamforgames6@gmail.com>
2022-09-17 19:33:02 -0700

    get renderer component idk

--- ClearGlass/ClearGlass.cs
+++ ClearGlass/ClearGlass.cs
@@ -15,11 +15,11 @@ namespace ClearGlass
                 if (loadScene != OWScene.SolarSystem) return;     
                 
                 // get some nice glass materials
-                var clearGlassMat = GameObject.Find("Ship_Body/Module_Cockpit/Geo_Cockpit/Cockpit_Geometry/Cockpit_Interior/Cockpit_TransparentGlass_Structural").GetComponent<MeshRenderer>().sharedMaterial;
-                var nomaiianClearGlassMat = GameObject.Find("CaveTwin_Body/Sector_CaveTwin/Interactables_CaveTwin/Structure_NOM_EyeSymbol/Structure_NOM_EyeSymbol_Glass").GetComponent<MeshRenderer>().sharedMaterials[1];
+                var clearGlassMat = GameObject.Find("Ship_Body/Module_Cockpit/Geo_Cockpit/Cockpit_Geometry/Cockpit_Interior/Cockpit_TransparentGlass_Structural").GetComponent<Renderer>().sharedMaterial;
+                var nomaiianClearGlassMat = GameObject.Find("CaveTwin_Body/Sector_CaveTwin/Interactables_CaveTwin/Structure_NOM_EyeSymbol/Structure_NOM_EyeSymbol_Glass").GetComponent<Renderer>().sharedMaterials[1];
                 
                 // clear up all other glass
-                foreach(var renderer in Resources.FindObjectsOfTypeAll<MeshRenderer>())
+                foreach(var renderer in Resources.FindObjectsOfTypeAll<Renderer>())
                 {
                     var materials = renderer.sharedMaterials;
                     for (var i = 0; i < materials.Length; i++)
