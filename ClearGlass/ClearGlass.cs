b5dac1955ee56cef14a42c3a2da97e3ed4789252
JohnCorby <williamforgames6@gmail.com>
2022-09-28 19:52:24 -0700

    TODO

--- ClearGlass/ClearGlass.cs
+++ ClearGlass/ClearGlass.cs
@@ -17,6 +17,7 @@ namespace ClearGlass
                 // get some nice glass materials
                 var clearGlassMat = GameObject.Find("Ship_Body/Module_Cockpit/Geo_Cockpit/Cockpit_Geometry/Cockpit_Interior/Cockpit_TransparentGlass_Structural").GetComponent<Renderer>().sharedMaterial;
                 var nomaiianClearGlassMat = GameObject.Find("CaveTwin_Body/Sector_CaveTwin/Interactables_CaveTwin/Structure_NOM_EyeSymbol/Structure_NOM_EyeSymbol_Glass").GetComponent<Renderer>().sharedMaterials[1];
+                // TODO: do artifact lens glass as well :D
                 
                 // clear up all other glass
                 foreach(var renderer in Resources.FindObjectsOfTypeAll<Renderer>())
